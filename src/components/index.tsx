82143c334c9ff4c452f5b8f7740c4fa41f281b1b
Alonso Diaz <alonsodiazlip@gmail.com>
2022-10-10 01:41:06 -0300

    deploy

--- src/components/index.tsx
+++
@@ -1,8 +0,0 @@
-export { BoardHeader } from "./BoardHeader";
-export { Modal } from "./Modal";
-export { HowToPlay } from "./HowToPlay";
-export { WordBox } from "./WordBox";
-export { RowWordsBoxs } from "./RowWordsBoxs";
-export { VirtualKeyBoard } from "./VirtualKeyBoard";
-export { Statistics } from "./Statistics";
-export { ThemeToggler } from "./ThemeToggler";
--- src/constants/index.ts
+++
@@ -1,8 +0,0 @@
-export const keysCapsValid = [
-  ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
-  ["A", "S", "D", "F", "G", "H", "J", "K", "L", "Ñ"],
-  ["ENTER", "Z", "X", "C", "V", "B", "N", "M", "\u232b"],
-];
-
-export const API_URL_WORDS =
-  "https://gitlab.com/api/v4/projects/33862155/repository/files/words.txt/raw?ref=main";
